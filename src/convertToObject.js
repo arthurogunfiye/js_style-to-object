4c0a7198920c105a3c98bca0b49ed32834a59458
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-03-11 07:14:36 +0000

    style-to-object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,14 +8,19 @@
 function convertToObject(sourceString) {
   const styleObject = {};
 
-  // Split by semicolons, filter out empty lines, and trim whitespace
-  sourceString.split(';').forEach((line) => {
-    const [property, value] = line.split(':');
+  // Split by semicolons, filter out empty
+  // or whitespace-only lines, and trim whitespace
+  sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line)
+    .forEach((line) => {
+      const [property, value] = line.split(':');
 
-    if (property && value) {
-      styleObject[property.trim()] = value.trim();
-    }
-  });
+      if (property && value) {
+        styleObject[property.trim()] = value.trim();
+      }
+    });
 
   return styleObject;
 }
