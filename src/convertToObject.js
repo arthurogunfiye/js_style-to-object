7a56452a85e95b5402cc6453aef13ab5a0b417c5
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-03-11 06:57:34 +0000

    style-to-object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+
+  // Split by semicolons, filter out empty lines, and trim whitespace
+  sourceString.split(';').forEach((line) => {
+    const [property, value] = line.split(':');
+
+    if (property && value) {
+      styleObject[property.trim()] = value.trim();
+    }
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;
